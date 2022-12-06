327d2f780f09d00765ee28da7f0e2ef63d890f57
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2022-12-06 20:14:37 +0700

    remove un use variable

--- projects/ng-ui/src/lib/ng-ui.component.ts
+++ projects/ng-ui/src/lib/ng-ui.component.ts
@@ -1,15 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'nu-ng-ui',
-  template: `
-    <p>
-      ng-ui works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>ng-ui works!</p> `,
+  styles: [],
 })
-export class NgUiComponent {
+export class NgUiComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
