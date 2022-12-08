9bb3c1fbe5e0deffacd46b1e01c21e1f68c3cf02
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2022-12-08 17:33:46 +0700

    feat: adding button stroked

--- projects/pattern-lib/src/lib/pattern-lib.module.ts
+++ projects/pattern-lib/src/lib/pattern-lib.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ButtonComponent } from './button/button.component';
 import { ButtonRaisedComponent } from './button-raised/button-raised.component';
+import { ButtonStrokedComponent } from './button-stroked/button-stroked.component';
 
 @NgModule({
-  declarations: [ButtonComponent, ButtonRaisedComponent],
+  declarations: [
+    ButtonComponent,
+    ButtonRaisedComponent,
+    ButtonStrokedComponent,
+  ],
   imports: [MatButtonModule],
   exports: [ButtonComponent, ButtonRaisedComponent],
 })
